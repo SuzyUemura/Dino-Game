87e78b484b949c7b7817831fc6f0a3699f652e82
SuzyUemura <kawany.suzy@gmail.com>
2021-01-06 23:01:51 -0300

    Desafio dino-game

---
+++ README.md
@@ -0,0 +1,19 @@
+### Desafio DIO: Reprodução do Game do Dinossauro sem intenet :video_game:
+
+#### Diferenciais:
+
+- Som no salto do dinoussauro 
+- Som de 'Game over' quando o usuário perde
+
+#### Linguagens utilizadas:
+
+- Html
+- JavaScript
+- CSS
+
+IDE: Sublime Text 3
+
+
+
+
+
--- scrip.js
+++ scrip.js
@@ -1,7 +1,10 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+
 let isJumpig = false;
 let position = 0;
+var audio = new Audio ('som.wav');
+var perdeu = new Audio ('robo_gameover.wav');
 
 function handleKeyUp(event) {
 	if (event.keyCode === 32) {
@@ -33,6 +36,7 @@ function jump() {
 		//subindo
 		position += 20;
 		dino.style.bottom = position + 'px';
+		audio.play();
 		}
 	}, 20);
 }
@@ -40,7 +44,7 @@ function jump() {
 function createCactus (){
 	const cactus = document.createElement('div');
 	let cactusPosition = 1000;
-	let randomTime = Math.random() * 6000;
+	let randomTime = Math.random() * 5000;
 
 	cactus.classList.add('cactus');
 	cactus.style.left = 1000 + 'px';
@@ -52,18 +56,20 @@ function createCactus (){
 			background.removeChild (cactus); //evita processamento desnecessario
 
 		} else if(cactusPosition > 0 && cactusPosition < 60 && position < 60) {
-		// Game Over
+		// Game Over	
 		clearInterval (leftInterval);
-		document.body.innerHTML = '<h1 class = "game-over">Fim de Jogo</h1'
-
+		document.body.innerHTML = '<h1 class = "game-over"> G A M E - O V E R </h1'
+		perdeu.play(); 
+		perdeu = null;
 		} else {
 			cactusPosition -= 10;
 			cactus.style.left = cactusPosition + 'px';
 		}
+
 	}, 20);
 
 	setTimeout (createCactus, randomTime); // função 'espelho'
-}
 
+}
 createCactus();
 document.addEventListener ('keyup', handleKeyUp);
